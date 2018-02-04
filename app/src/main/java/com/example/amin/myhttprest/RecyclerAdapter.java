52aeadd240e591095f99c2d1912b8fbc4ac1c9d8
Amin Said <Amin-Said@users.noreply.github.com>
2018-02-04 05:10:20 +0200

    displaying text results and image in a recycle view

--- app/src/main/java/com/example/amin/myhttprest/RecyclerAdapter.java
+++ app/src/main/java/com/example/amin/myhttprest/RecyclerAdapter.java
@@ -1,8 +1,8 @@
 package com.example.amin.myhttprest;
 
+import android.view.LayoutInflater;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
